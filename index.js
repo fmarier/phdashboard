9bf523c830db7656921f85666715d855c93598a5
Francois Marier <francois@mozilla.com>
2012-11-12 18:36:16 +1300

    Move config blurbs into the module files

--- index.js
+++ index.js
@@ -11,42 +11,10 @@ github = require('./lib/github.js'),
 teamstatus = require('./lib/teamstatus.js');
 
 conf = convict({
-  asana: {
-    apiKey: {
-      doc: "API key",
-      format: 'string'
-    },
-    workspaces: {
-      doc: "List of workspace IDs to use, separated by commas.",
-      format: 'string'
-    }
-  },
-  bugzilla: {
-    email: {
-      doc: "Bugzilla email",
-      format: 'string'
-    }
-  },
-  github: {
-    username: {
-      doc: "Github username",
-      format: 'string'
-    }
-  },
-  teamstatus: {
-    server: {
-      doc: "IRC server",
-      format: 'string = "irc.mozilla.org"'
-    },
-    channel: {
-      doc: "IRC channel",
-      format: 'string = "identity"'
-    },
-    nick: {
-      doc: "IRC nickname",
-      format: 'string'
-    }
-  }
+  asana: asana.config,
+  bugzilla: bugzilla.config,
+  github: github.config,
+  teamstatus: teamstatus.config
 }).loadFile(__dirname + '/config.json').validate();
 
 // TODO: check https certs
